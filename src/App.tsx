c9c5c35068e9a59fd4cc0ad0f4c68854e9bec2a2
Michaelmwaringa <acadeemiasms@gmail.com>
2026-01-30 16:23:08 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import AboutPage from './pages/About';
 import ServicesPage from './pages/Services';
 import TeamPage from './pages/Team';
 import ContactPage from './pages/Contact';
+import PrivacyPolicyPage from './pages/PrivacyPolicy';
+import TermsOfServicePage from './pages/TermsOfService';
+import CookiePolicyPage from './pages/CookiePolicy';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/team" element={<TeamPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+          <Route path="/cookie-policy" element={<CookiePolicyPage />} />
         </Routes>
       </Layout>
     </Router>
